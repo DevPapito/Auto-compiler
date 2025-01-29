ea91d61d3df29186862eddc724ad08de5ff92d9e
Pietro Ezio <pietroeziosuzano@gmail.com>
2025-01-29 01:44:12 -0300

    Add structure project

---
+++ principalApp/App.py
@@ -0,0 +1,14 @@
+
+
+class App():
+
+    @classmethod
+    def main(cls):
+        raise Exception("Inicio do programam")
+    
+    @staticmethod
+    def creatureString(string:str):
+        return string
+
+if __name__ == '__main__':
+    App().main()
